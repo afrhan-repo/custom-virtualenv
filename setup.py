5f38bbd3f7ee172107e59197860d0d73df1dc010
afrhan-repo <afrhanhossain11@gmail.com>
2024-10-05 15:46:28 +0530

    Pipdeptree dependency added in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="custom-virtualenv",
-    version="1.0.1",
+    version="1.0.2",
     description="Custom Virtual Environment Manager is a utility that allows you to create virtual environments with packages of your choice, based on the Python packages installed globally on your system.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -18,5 +18,5 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.7",
-    install_requires=["InquirerPy", "wheel"],
+    install_requires=["InquirerPy","pipdeptree"],
 )
